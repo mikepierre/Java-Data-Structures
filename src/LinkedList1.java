f469298f6900a5b61f5cada35c29006034465755
mikepierre <mikeypierre84@yahoo.com>
2016-01-07 17:47:55 -0500

    Linked list Single Example 1

---
+++ src/LinkedList1.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LinkedList<String> ll = new LinkedList<String>();
+		ll.add("item 1");
+		ll.add("item 2");
+		
+		System.out.println("Size of List: "+ ll.size());
+		
+	}
+
+}
