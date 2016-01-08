8bb53360ad5958346d811bbc6a82056e14967337
mikepierre <mikeypierre84@yahoo.com>
2016-01-07 19:35:52 -0500

    Added some items to list

--- src/LinkedList1.java
+++ src/LinkedList1.java
@@ -4,13 +4,23 @@ import java.util.LinkedList;
 public class LinkedList1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		LinkedList<String> ll = new LinkedList<String>();
 		ll.add("item 1");
 		ll.add("item 2");
 		
 		System.out.println("Size of List: "+ ll.size());
+		
+		System.out.println("Content in LList:" + ll);
+		
+		ll.add("item 3");
+		
+		
+		System.out.println("New List" + ll);
+	
+		for(int i = 0; i < ll.size(); i++){
+			System.out.println("Result "+ i +" " + ll.get(i));
+		}
 		
 	}
 
