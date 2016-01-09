96dbc4b2f087501df30819e8dd2a7884a38151cb
mikepierre <mikeypierre84@yahoo.com>
2016-01-09 00:07:20 -0500

    Updates

--- src/MinStack.java
+++ src/MinStack.java
@@ -5,22 +5,25 @@ public class MinStack  {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 
 	}
 
-	public static void push(int value){
+	public int push(int value){
 		int newMin = Math.min(value, min());
-		super.push("");
+		return newMin;
 	}
 	
-	public static int min(){
+	public int min(){
 		if(this.isEmpty()){
 			return Integer.MAX_VALUE;
 		} else {
-			return peek().min;
+			return Integer.MIN_VALUE;
 		}
 
+	}
+	private boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
