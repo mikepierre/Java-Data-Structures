a33f5e1c04a881d34c250f2263b3811c5aede86a
mikepierre <mikeypierre84@yahoo.com>
2017-07-17 22:10:04 -0400

    House Keeping

--- src/HashTable1.java
+++ src/HashTable1.java
@@ -1,10 +1,8 @@
 import java.util.Hashtable;
 
-
 public class HashTable1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Hashtable<String, Double> availability = new Hashtable<String, Double>();
 		
